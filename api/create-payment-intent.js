71fd22cf8e14ae03c1bdc15495591e916e30a490
Lbourgeois98 <73048129+Lbourgeois98@users.noreply.github.com>
2025-08-20 15:31:00 -0600

    Update create-payment-intent.js

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -2,22 +2,24 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async (req, res) => {
-  if (req.method === 'POST') {
-    try {
-      const { amount = 50 } = req.body;
-
-      const intent = await stripe.paymentIntents.create({
-        amount,
-        currency: 'usd',
-        automatic_payment_methods: { enabled: true },
-      });
-
-      res.status(200).json({ clientSecret: intent.client_secret });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  } else {
+  if (req.method !== 'POST') {
     res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    return res.status(405).end('Method Not Allowed');
+  }
+
+  try {
+    const { amount = 50 } = req.body;
+
+    const intent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+      payment_method_types: ['card'],
+      capture_method: 'automatic',
+      confirm: false
+    });
+
+    res.status(200).json({ clientSecret: intent.client_secret });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
