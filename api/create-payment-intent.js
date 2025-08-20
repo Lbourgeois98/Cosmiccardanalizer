6ea6ba2d150834ab74147ec5f43ccc49eb593183
Lbourgeois98 <73048129+Lbourgeois98@users.noreply.github.com>
2025-08-20 14:25:25 -0600

    Update create-payment-intent.js

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,27 +1,20 @@
+// api/create-payment-intent.js
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-module.exports = async function (req, res) {
+module.exports = async (req, res) => {
   if (req.method === 'POST') {
-    const { amount, payment_method } = req.body;
-
     try {
-      const intent = await stripe.paymentIntents.create({
-        amount: amount,
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: 50,
         currency: 'usd',
-        payment_method: payment_method,
-        confirmation_method: 'automatic',
-        confirm: true,
-        return_url: 'https://cardanalyzer.techpimp.site',
-        capture_method: 'manual' // So you can cancel later
+        automatic_payment_methods: { enabled: true },
       });
 
-      if (intent.status === 'requires_capture' || intent.status === 'succeeded') {
-        res.status(200).json({ success: true, clientSecret: intent.client_secret });
-      } else {
-        res.status(400).json({ success: false, error: intent.last_payment_error?.message || 'Card not authorized' });
-      }
+      res.status(200).send({
+        clientSecret: paymentIntent.client_secret,
+      });
     } catch (err) {
-      res.status(400).json({ success: false, error: err.message });
+      res.status(500).send({ error: err.message });
     }
   } else {
     res.setHeader('Allow', 'POST');
